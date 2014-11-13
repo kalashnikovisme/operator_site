48619746a86d28921bae365b065d9e5258264010
pavel <kalashnikovisme@gmail.com>
2014-11-13 18:02:42 +0300

    fix test for main page

--- test/controllers/web/welcome_controller_test.rb
+++ test/controllers/web/welcome_controller_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class Web::WelcomeControllerTest < ActionController::TestCase
   test 'should get index' do
+    main_page = create :page
+    main_page.slug = 'welcome'
+    main_page.save
     get :index
     assert_response :success, @response.body
   end
