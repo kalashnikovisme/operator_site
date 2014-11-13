4a851d4808966be492bbd8a5a0e5bf2875c2d928
pavel <kalashnikovisme@gmail.com>
2014-11-13 18:43:29 +0300

    fix video on main page and add locales

--- app/decorators/video_decorator.rb
+++ app/decorators/video_decorator.rb
@@ -3,10 +3,10 @@ class VideoDecorator < Draper::Decorator
 
   include AutoHtml
 
-  def from_youtube(width = 350, height = 197)
+  def from_youtube(width = 800, height = 455)
     if model.link
       auto_html model.link do
-        youtube width: width, height: height
+        youtube width: width, height: height, autoplay: 1
       end
     end
   end
