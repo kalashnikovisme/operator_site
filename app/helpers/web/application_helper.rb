969418ced3b7ccd3fccd8c2ac21f539fd96fc3f0
pavel <kalashnikovisme@gmail.com>
2015-01-10 09:17:51 +0300

    some adds to footer

--- app/helpers/web/application_helper.rb
+++ app/helpers/web/application_helper.rb
@@ -21,4 +21,16 @@ module Web::ApplicationHelper
   def localize_enum(value, attr_name, model_name)
     I18n.t("enumerize.#{model_name}.#{attr_name}.#{value}")
   end
+
+  def copyright_notice_year_range(start_year)
+    start_year = start_year.to_i
+    current_year = Time.new.year
+    if current_year > start_year && start_year > 2000
+      "#{start_year} - #{current_year}"
+    elsif start_year > 2000
+      "#{start_year}"
+    else
+      "#{current_year}"
+    end
+  end
 end
