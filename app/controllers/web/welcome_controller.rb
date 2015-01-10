d6ff5e0ce73aa53386428522dd7cef8c55f745c8
pavel <kalashnikovisme@gmail.com>
2015-01-10 08:58:42 +0300

    get shuffle banner

--- app/controllers/web/welcome_controller.rb
+++ app/controllers/web/welcome_controller.rb
@@ -2,7 +2,7 @@ class Web::WelcomeController < Web::ApplicationController
   def index
     @page = Page.find_by_slug :welcome
     @members = Member.all
-    @banner = Banner.active.first
+    @banner = Banner.active.shuffle.first
     @videos = VideoDecorator.decorate_collection Video.all
     @project_types = Video.project_type.values
     @user = UserEditType.new
