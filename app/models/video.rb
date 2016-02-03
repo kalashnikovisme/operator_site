ca688af1d9c0ac8e3846d20c4e840d187c98a6c9
pavel <kalashnikovisme@gmail.com>
2016-02-03 23:32:46 +0300

    move videos by order_number

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,8 +5,7 @@ class Video < ActiveRecord::Base
   validates :description, presence: true
   validates :project_type, presence: true
   validates :order_number, presence: true,
-                           numericality: { greater_than: 0 },
-                           uniqueness: { scope: :project_type }
+                           numericality: { greater_than: 0 }
 
   include VideoRepository
   extend Enumerize
